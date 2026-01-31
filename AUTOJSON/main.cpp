a35dda10e008fb3312187878192e5a874b06343f
Torchor <torch@gmail.com>
2026-01-31 16:47:41 +0800

    Initial Commit

---
+++ AUTOJSON/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AUTOJSON
+//
+//  Created by Marvel on 2026/1/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
