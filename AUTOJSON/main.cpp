982019377c812574fb5975e1733fcd35c49ed3da
Torchor <torch@gmail.com>
2026-01-31 22:30:38 +0800

    1

--- AUTOJSON/main.cpp
+++ AUTOJSON/main.cpp
@@ -11,12 +11,12 @@
 
 
 
-#define __ADD__JSON__KEY__1(MODEL,_1)  {#_1,{offsetof(MODEL, _1),(auto_json::pFuncParse)auto_json::transform<decltype(MODEL::_1)>::from_json}},
+#define __ADD__JSON__KEY__1(C,_1)  {#_1,{offsetof(C, _1),(auto_json::pFuncParse)auto_json::transform<decltype(C::_1)>::from_json}},
 
 
-#define __ADD__JSON__KEY__2(MODEL,_1,_2)      __ADD__JSON__KEY__1(MODEL,_1) __ADD__JSON__KEY__1(MODEL,_2)
-#define __ADD__JSON__KEY__3(MODEL,_1,_2,_3)   __ADD__JSON__KEY__2(MODEL,_1,_2) __ADD__JSON__KEY__1(MODEL,_3)
-#define __ADD__JSON__KEY__4(MODEL,_1,_2,_3,_4)   __ADD__JSON__KEY__3(MODEL,_1,_2,_3) __ADD__JSON__KEY__1(MODEL,_4)
+#define __ADD__JSON__KEY__2(C,_1,_2)      __ADD__JSON__KEY__1(C,_1) __ADD__JSON__KEY__1(C,_2)
+#define __ADD__JSON__KEY__3(C,_1,_2,_3)   __ADD__JSON__KEY__2(C,_1,_2) __ADD__JSON__KEY__1(C,_3)
+#define __ADD__JSON__KEY__4(C,_1,_2,_3,_4)   __ADD__JSON__KEY__3(C,_1,_2,_3) __ADD__JSON__KEY__1(C,_4)
 
 
 namespace auto_json {
@@ -121,8 +121,6 @@ inline bool transform_from_json(const std::string &json){return auto_json::trans
 
 
 
-
-#define IMPLEMENT__JSON__AUTO__TRANSFORM(MODEL,...)
 
 struct JOSONODEL {
     int a{};
