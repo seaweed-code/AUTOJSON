26865eca953005676c06595235734bf8c49fff3c
Torchor <torch@gmail.com>
2026-01-31 19:36:54 +0800

    Update main.cpp

--- AUTOJSON/main.cpp
+++ AUTOJSON/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  AUTOJSON
 //
-//  Created by Marvel on 2026/1/31.
+//  Created by Matthew on 2026/1/31.
 //
 
 #include <iostream>
@@ -18,11 +18,11 @@ using pFuncParse = bool(*)(const char*key,const std::string &json,void*pThis,voi
 using ReflectMapType = std::unordered_map<const char*,std::pair<OffsetType,pFuncParse>>;
 
 template <typename  T>
-bool transfom_from_json(const char*key,const std::string &json,void*pThis,T * offset);
+bool transform_from_json(const char*key,const std::string &json,void*pThis,T * offset);
 
 
 template <>
-bool transfom_from_json<bool>(const char*key,const std::string &json,void*pThis,bool * offset)
+bool transform_from_json<bool>(const char*key,const std::string &json,void*pThis,bool * offset)
 {
     auto && dest = *reinterpret_cast<bool*>(static_cast<char*>(pThis) + reinterpret_cast<OffsetType>(offset));
     dest = true;
@@ -32,7 +32,7 @@ bool transfom_from_json<bool>(const char*key,const std::string &json,void*pThis,
 }
 
 template <>
-bool transfom_from_json<int>(const char*key,const std::string &json,void*pThis,int * offset)
+bool transform_from_json<int>(const char*key,const std::string &json,void*pThis,int * offset)
 {
     auto && dest = *reinterpret_cast<int*>(static_cast<char*>(pThis) + reinterpret_cast<OffsetType>(offset));
     dest = 1;
@@ -42,7 +42,7 @@ bool transfom_from_json<int>(const char*key,const std::string &json,void*pThis,i
 }
 
 template <>
-bool transfom_from_json<double>(const char*key,const std::string &json,void*pThis,double * offset)
+bool transform_from_json<double>(const char*key,const std::string &json,void*pThis,double * offset)
 {
     auto && dest = *reinterpret_cast<double*>(static_cast<char*>(pThis) + reinterpret_cast<OffsetType>(offset));
     dest = 9.9;
@@ -51,7 +51,7 @@ bool transfom_from_json<double>(const char*key,const std::string &json,void*pThi
 }
 
 template <>
-bool transfom_from_json<std::string>(const char*key,const std::string &json,void*pThis,std::string * offset)
+bool transform_from_json<std::string>(const char*key,const std::string &json,void*pThis,std::string * offset)
 {
     auto && dest = *reinterpret_cast<std::string*>(static_cast<char*>(pThis) + reinterpret_cast<OffsetType>(offset));
     dest = "9099009llkk";
@@ -59,7 +59,7 @@ bool transfom_from_json<std::string>(const char*key,const std::string &json,void
     return true;
 }
 
-bool transform_to_model(void *pThis,const auto_json::ReflectMapType &reflect_map ,const std::string&json)
+bool transform_from_json(void *pThis,const auto_json::ReflectMapType &reflect_map ,const std::string&json)
 {
     for (auto&& p:reflect_map)
     {
@@ -81,14 +81,14 @@ struct JOSONODEL {
     std::vector<int> dd;
 
     const static auto_json::ReflectMapType reflect_map;
-    inline bool transform_from_json(const std::string &json){return auto_json::transform_to_model(this, reflect_map, json);}
+    inline bool transform_from_json(const std::string &json){return auto_json::transform_from_json(this, reflect_map, json);}
     
 };
 
 const  auto_json::ReflectMapType JOSONODEL::reflect_map = {
-    {TO_STRING(a),{offsetof(JOSONODEL, a),(auto_json::pFuncParse)auto_json::transfom_from_json<int>}},
-    {TO_STRING(b),{offsetof(JOSONODEL, b),(auto_json::pFuncParse)auto_json::transfom_from_json<double>}},
-    {TO_STRING(cc),{offsetof(JOSONODEL, cc),(auto_json::pFuncParse)auto_json::transfom_from_json<std::string>}},
+    {TO_STRING(a),{offsetof(JOSONODEL, a),(auto_json::pFuncParse)auto_json::transform_from_json<int>}},
+    {TO_STRING(b),{offsetof(JOSONODEL, b),(auto_json::pFuncParse)auto_json::transform_from_json<double>}},
+    {TO_STRING(cc),{offsetof(JOSONODEL, cc),(auto_json::pFuncParse)auto_json::transform_from_json<std::string>}},
 };
 
 int main(int argc, const char * argv[]) {
