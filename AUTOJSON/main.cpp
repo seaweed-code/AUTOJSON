584011cebf0fea0a092f08087b048bfc5a6bc7f3
Torchor <torch@gmail.com>
2026-02-01 11:36:33 +0800

    Update main.cpp

--- AUTOJSON/main.cpp
+++ AUTOJSON/main.cpp
@@ -8,7 +8,9 @@
 
 struct AA
 {
-    int aa;
+    int id;
+    std::string name;
+    bool online;
     DECLARE__JSON__AUTO__TRANSFORM
 };
 
@@ -28,18 +30,16 @@ struct JOSONODEL {
     int a6{};
     bool aaa000;
     
-    AA custom;
-    
-    
+    AA users;
     
     DECLARE__JSON__AUTO__TRANSFORM
 };
 
-IMPLEMENT__JSON__AUTO__TRANSFORM(JOSONODEL,a,b,url,dd,name,id,a3,a4,a5,a6,aaa000,custom,boosaa)
-IMPLEMENT__JSON__AUTO__TRANSFORM(AA,aa)
+IMPLEMENT__JSON__AUTO__TRANSFORM(JOSONODEL,a,b,url,dd,name,id,a3,a4,a5,a6,aaa000,users,boosaa)
+IMPLEMENT__JSON__AUTO__TRANSFORM(AA,id,name,online)
 
 int main(int argc, const char * argv[]) {
-    std::string json = "{\"id\": 1,    \"name\": \"mmahew\",  \"boosaa\": [\"---8.8\", \"ppp9.9\", \"kkk0.800\"],  \"url\": \"www.runoob.com\"}";
+    std::string json = "{\"id\": 1,    \"name\": \"mmahew\", \"users\": [{ \"id\": 100, \"name\": \"Tom\", \"online\": true },    { \"id\": 101, \"name\": \"Jerry\", \"online\": false } ],  \"boosaa\": [\"---8.8\", \"ppp9.9\", \"kkk0.800\"],  \"url\": \"www.runoob.com\"}";
     
     JOSONODEL aa;
     
